02194e8077f0decbe59eb5c8a2e75cd4f02da96f
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-08 22:27:05 +0200

    Done lesson 15

--- primeapp/main.go
+++ primeapp/main.go
@@ -1,12 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 2
+	// print welcome message
+	intro()
 
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	// channel to indicate user quits
+	doneChan := make(chan bool)
+
+	// start goroutine to read user input
+	go readUserInput(doneChan)
+
+	// wait for channel value
+	<-doneChan
+
+	// close channel
+	close(doneChan)
+
+	// say goodbye
+	fmt.Println("Ciao!")
+}
+
+func checkNumber(s *bufio.Scanner) (string, bool) {
+	s.Scan()
+	if strings.EqualFold(s.Text(), "q") {
+		return "", true
+	}
+	numToCheck, err := strconv.Atoi(s.Text())
+	if err != nil {
+		return "Please enter a whole number!", false
+	}
+
+	_, msg := isPrime(numToCheck)
+
+	return msg, false
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumber(scanner)
+		if done {
+			doneChan <- true
+			return
+		}
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func intro() {
+	fmt.Println("Is Prime?")
+	fmt.Println("=========")
+	fmt.Println("Enter a whole number")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("-> ")
+
 }
 
 func isPrime(n int) (bool, string) {
